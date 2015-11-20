a6766bcc2cbe87af591193265d6c64326716ee1a
vitorarins <vitorarins@gmail.com>
2015-11-20 16:58:10 -0200

    Added alias to remove none images

--- custom/example.zsh
+++ custom/example.zsh
@@ -4,3 +4,4 @@
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
 #
 zstyle ':completion:*' special-dirs true
+alias remove-docker-nones='docker rmi $(docker images | grep "^<none>" | awk "{print $3}")'
