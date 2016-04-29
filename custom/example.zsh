3521fc515af0b1090e7db221f161b2df5624dc41
vitorarins <vitorarins@gmail.com>
2016-04-29 17:47:43 -0300

    Added changes.

--- custom/example.zsh
+++ custom/example.zsh
@@ -4,4 +4,11 @@
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
 #
 zstyle ':completion:*' special-dirs true
-alias remove-docker-nones='docker rmi $(docker images | grep "^<none>" | awk "{print $3}")'
+alias remove-docker-nones="docker rmi -f $(docker images | grep '^<none>' | awk '{print $3}')"
+
+removeImages() {
+    docker rmi -f $(docker images | grep $1 | awk '{print $3}')
+}
+alias remove-docker-images=removeImages
+    
+alias tabula='docker run -it --rm -v $PWD:/usr/src/app -w /usr/src/app/ vitorarins/tabula-extractor tabula'
